754acf05fc963472b63a08e2363245ffb3a51a25
unknown <it17094078@my.sliit.lk>
2020-07-13 22:49:01 +0530

    Daily Auto commit

--- dailycommit.sh
+++ dailycommit.sh
@@ -1,11 +1,9 @@
 ls -all > github.log
-sleep 2s
 echo "Daily github auto commit"
 git add .
 git commit -m "Daily Auto commit"
 git push
 rm github.log
-sleep 2s
 git add .
 git commit -m "Daily Auto commit"
 git push
