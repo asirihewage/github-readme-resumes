c58ca5db352dc684e48c2bf4d17919f4be196879
unknown <it17094078@my.sliit.lk>
2020-07-13 22:31:50 +0530

    Daily Auto commit

--- dailycommit.sh
+++ dailycommit.sh
@@ -1,5 +1,5 @@
 cd G:/GitHub/github-readme-resumes/
-mv github.log github_$(date +%d-%m-%Y).log
+mv github_$(yesterday +%d-%m-%Y).log github_$(date +%d-%m-%Y).log
 git add .
 git commit -m "Daily Auto commit"
 git push
