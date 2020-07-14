56233a86de171764e6aa48bff002c54a8f3dca1a
unknown <it17094078@my.sliit.lk>
2020-07-14 17:31:31 +0530

    Daily Auto commit 07/14/20

--- dailycommit.sh
+++
@@ -1,8 +0,0 @@
-ls -all > github.log
-echo "Daily github auto commit `date +%D`"
-git add .
-git commit -m "Daily Auto commit `date +%D`"
-rm github.log
-git add .
-git commit -m "Daily Auto commit `date +%D`"
-git push
